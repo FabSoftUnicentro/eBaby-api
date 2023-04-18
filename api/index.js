45e05b7e2d61fbd57779daf1b9e42681430a546d
Restyled.io <commits@restyled.io>
2023-04-18 00:16:28 +0000

    Restyled by clang-format

--- api/index.js
+++ api/index.js
@@ -1,6 +1,4 @@
 const app = require('../server')
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log("Servidor Rodando!")
-})
+app.listen(port, () => {console.log("Servidor Rodando!")})
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const app = require("express")();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/?retryWrites=true&w=majority`;
+const uri =
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${
+        process.env.MONGO_HOST}/?retryWrites=true&w=majority`;
 
 // Conecta no MongoDB
 mongoose.Promise = global.Promise;
-mongoose.connect(uri, {useNewUrlParser: true}).then(() => {
-    console.log("Conectado ao mongo")
-  }).catch((err) => {
-    console.log("Erro ao se conectar: "+err)
-  })
+mongoose.connect(uri, {useNewUrlParser : true})
+    .then(() => {console.log("Conectado ao mongo")})
+    .catch((err) => {console.log("Erro ao se conectar: " + err)})
 
 // Carrega o model de Usuário
 require("./models/user");

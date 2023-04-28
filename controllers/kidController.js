b2c348fcd58c93a499f54e28a52ddfcec980a953
Daniel Volski <danfvdaum3@hotmail.com>
2023-04-28 20:26:06 -0300

    Fixed the registerKid route by adding the missing data

--- controllers/kidController.js
+++ controllers/kidController.js
@@ -5,17 +5,28 @@ const authMiddleware = require("../middlewares/auth");
 
 const Kid = mongoose.model("Kid");
 
+router.use(authMiddleware);
+
 router.post("/registerKid", async (req, res) => {
-    const { cpf, name, dateOfBirth, gestationalAge, weight, length} = req.body;
-  
+    const kidData = {
+      cpf: req.body.kid[0]["cpf"],
+      name: req.body.kid[0]["name"],
+      dateOfBirth: req.body.kid[0]["birthAge"],
+      gestationalAge: req.body.kid[0]["gestationalAge"],
+      weight: req.body.testkid[0]["weight"],
+      length: req.body.testkid[0]["length"],
+    };
+
     try {
-      if (await Kid.findOne({ cpf })) {
+      if (await Kid.findOne({ kidData })) {
         return res.send({'success': false, 'Message': "Email já cadastrado :("});
       }
   
-      const kid = await Kid.create(req.body);
-      return res.json({ kid });
+      const kid = await Kid.create(kidData);
+
+      return res.json({ kid, 'success': true });
     } catch (err) {
+      console.error(err)
       return res.status(400).json({ error: "Kid registration failed" });
     }
   });

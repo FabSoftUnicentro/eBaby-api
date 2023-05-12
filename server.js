fec797db29b11eae0e7bb1c9c56258410fd75556
Restyled.io <commits@restyled.io>
2023-05-12 15:45:05 +0000

    Restyled by prettier

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const app = require("express")();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const uri = 'mongodb+srv://dan:1234@ebaby.t0bujpo.mongodb.net/test';
+const uri = "mongodb+srv://dan:1234@ebaby.t0bujpo.mongodb.net/test";
 
 // Conecta no MongoDB
 mongoose.Promise = global.Promise;
-mongoose.connect(uri, {useNewUrlParser : true})
-    .then(() => {console.log("Conectado ao mongo")})
-    .catch((err) => {console.log("Erro ao se conectar: " + err)})
+mongoose
+  .connect(uri, { useNewUrlParser: true })
+  .then(() => {
+    console.log("Conectado ao mongo");
+  })
+  .catch((err) => {
+    console.log("Erro ao se conectar: " + err);
+  });
 
 // Carrega o model de Usuário
 require("./models/user");
@@ -22,6 +27,8 @@ app.use("/api", require("./controllers/userController"));
 app.use("/api", require("./controllers/kidController"));
 
 // Health Check
-app.get("/health", async (req, res) => { return res.json({message : "OK"}); });
+app.get("/health", async (req, res) => {
+  return res.json({ message: "OK" });
+});
 
 module.exports = app;

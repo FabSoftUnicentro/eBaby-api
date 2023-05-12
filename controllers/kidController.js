589db6ad71367e2d51e050a3772e23b66deaf345
Daniel Volski <danfvdaum3@hotmail.com>
2023-05-12 12:46:50 -0300

    Changing the post route registerKid to use the models test and testQuestion

--- controllers/kidController.js
+++ controllers/kidController.js
@@ -2,13 +2,14 @@ const router = require("express").Router();
 const mongoose = require("mongoose");
 const authMiddleware = require("../middlewares/auth");
 
-
 const Kid = mongoose.model("Kid");
+const Test = mongoose.model("Test");
+const TestQuestion = mongoose.model("TestQuestion");
 
 router.use(authMiddleware);
 
 router.post("/registerKid", async (req, res) => {
-    const kidData = {
+  const kidData = {
       cpf: req.body.kid[0]["cpf"],
       name: req.body.kid[0]["name"],
       dateOfBirth: req.body.kid[0]["birthAge"],
@@ -18,13 +19,29 @@ router.post("/registerKid", async (req, res) => {
     };
 
     try {
-      if (await Kid.findOne({ kidData })) {
-        return res.send({'success': false, 'Message': "Email já cadastrado :("});
+      let kid = await Kid.findOne({ cpf: kidData.cpf });
+      if (! kid) {
+        kid = await Kid.create(kidData);
       }
   
-      const kid = await Kid.create(kidData);
+      const test = await Test.create({
+        cpfKid: kidData.cpf,
+        cpfAgent: req.body.agent.cpf,
+        weight: req.body.testkid[0]["weight"],
+        length: req.body.testkid[0]["length"],
+        note: req.body.testkid[0]["note"],
+        done: req.body.testkid[0]["done"],
+      });
 
-      return res.json({ kid, 'success': true });
+      for (item of req.body.testkid[0].itens) {
+        await TestQuestion.create({
+          test_id: test._id,
+          question_id: item.id,
+          success: item.result,
+        });
+      }
+
+      return res.json({'success': true});
     } catch (err) {
       console.error(err)
       return res.status(400).json({ error: "Kid registration failed" });

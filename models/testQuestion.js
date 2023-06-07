eaffb4430bea397886b536a2b9e6428dc299c2d4
Restyled.io <commits@restyled.io>
2023-06-07 14:21:06 +0000

    Restyled by clang-format

--- models/testQuestion.js
+++ models/testQuestion.js
@@ -2,23 +2,20 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const TestQuestionSchema = new mongoose.Schema({
-    test_id: {
-        type: Schema.Types.ObjectId,
-        ref: 'Test',
-        required: true,
-    },
-    question_id: {
-        type: Number,
-        required: true,
-    },
-    success: {
-        type: Number,
-        required: true,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now
-    },
-  });
+  test_id : {
+    type : Schema.Types.ObjectId,
+    ref : 'Test',
+    required : true,
+  },
+  question_id : {
+    type : Number,
+    required : true,
+  },
+  success : {
+    type : Number,
+    required : true,
+  },
+  createdAt : {type : Date, default : Date.now},
+});
 
-  mongoose.model("TestQuestion", TestQuestionSchema);
+mongoose.model("TestQuestion", TestQuestionSchema);

419fec12fd87c3efc064c55cc82a8d9d2dae8675
Restyled.io <commits@restyled.io>
2023-06-07 14:21:08 +0000

    Restyled by prettier

--- models/testQuestion.js
+++ models/testQuestion.js
@@ -2,20 +2,20 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const TestQuestionSchema = new mongoose.Schema({
-  test_id : {
-    type : Schema.Types.ObjectId,
-    ref : 'Test',
-    required : true,
+  test_id: {
+    type: Schema.Types.ObjectId,
+    ref: "Test",
+    required: true,
   },
-  question_id : {
-    type : Number,
-    required : true,
+  question_id: {
+    type: Number,
+    required: true,
   },
-  success : {
-    type : Number,
-    required : true,
+  success: {
+    type: Number,
+    required: true,
   },
-  createdAt : {type : Date, default : Date.now},
+  createdAt: { type: Date, default: Date.now },
 });
 
 mongoose.model("TestQuestion", TestQuestionSchema);

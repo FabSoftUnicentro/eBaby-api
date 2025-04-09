09ef9f5acfa5c31123e0fc22a385aac673dd3d5c
joaobiava <138793503+joaobiava@users.noreply.github.com>
2025-04-09 19:53:29 -0300

    fix: adicionado transação e tratamento de dados

--- controllers/kidController.js
+++ controllers/kidController.js
@@ -10,43 +10,57 @@ router.use(authMiddleware);
 
 router.post("/registerKid", async (req, res) => {
   try {
+    const session = await mongoose.startSession();
 
     for (let i = 0; i < req.body.kid.length; i++) {
-      const kidData = {
-        cpf: req.body.kid[i]["cpf"],
-        name: req.body.kid[i]["name"],
-        dateOfBirth: req.body.kid[i]["birthAge"],
-        gestationalAge: req.body.kid[i]["gestationalAge"],
-        weight: req.body.testkid[i]["weight"],
-        length: req.body.testkid[i]["length"],
-      };
+      await session.withTransaction(async () => {
+        const kidData = {
+          cpf: req.body.kid[i]["cpf"],
+          name: req.body.kid[i]["name"],
+          dateOfBirth: req.body.kid[i]["birthAge"],
+          gestationalAge: req.body.kid[i]["gestationalAge"],
+          weight: req.body.testkid[i]["weight"],
+          length: req.body.testkid[i]["length"],
+        };
 
-      let kid = await Kid.findOne({ cpf: kidData.cpf });
-      if (!kid) {
-        kid = await Kid.create(kidData);
-      }
+        let kid = await Kid.findOne({ cpf: kidData.cpf }, null, { session: session });
+        if (!kid) {
+          kid = await Kid.create([kidData], { session: session });
+        }
 
-      const test = await Test.create({
-        cpfKid: kidData.cpf,
-        cpfAgent: req.body.agent.cpf,
-        weight: req.body.testkid[i]["weight"],
-        length: req.body.testkid[i]["length"],
-        note: req.body.testkid[i]["note"],
-        done: req.body.testkid[i]["done"],
+        const test = await Test.create(
+          [
+            {
+              cpfKid: kidData.cpf,
+              cpfAgent: req.body.agent.cpf,
+              weight: req.body.testkid[i]["weight"],
+              length: req.body.testkid[i]["length"],
+              note: req.body.testkid[i]["note"],
+              done: req.body.testkid[i]["done"],
+            }
+          ],
+          { session: session },
+        );
+
+        for (item of req.body.testkid[i].itens) {
+          await TestQuestion.create(
+            [
+              {
+                test_id: test[0]._id,
+                question_id: item.id,
+                success: item.result,
+              }
+            ],
+            { session: session },
+          );
+        }
       });
-
-      for (item of req.body.testkid[i].itens) {
-        await TestQuestion.create({
-          test_id: test._id,
-          question_id: item.id,
-          success: item.result,
-        });
-      }
     }
 
     return res.json({ success: true });
   } catch (err) {
     console.error(err);
+
     return res.status(400).json({ error: "Kid registration failed" });
   }
 });
